e5c3ff07f72caa8312f0ed29a6b26a43d9f6156f
Gavin Eke <GavinEke@users.noreply.github.com>
2016-03-15 00:15:08 +1100

    Update Get-WBReport.ps1

--- Get-WBReport.ps1
+++ Get-WBReport.ps1
@@ -1,22 +1,25 @@
-# PowerShell Windows Server Backup Report
-# Twitter: @GavinEke
-#
-# Requires Windows Server Backup Command Line Tools
-# Tested on Windows Server 2012 R2 using PowerShell 4.0
-# Example usage: .\Get-WBReport.ps1
+#Requires –Version 3
+#Requires –Modules WindowsServerBackup
 
-#region Varibles
-# You should change the variables in this region to suit your environment
+<#
+    PowerShell Windows Server Backup Report
+    Twitter: @GavinEke
+    
+    Requires Windows Server Backup Command Line Tools
+    This version is for Windows Server 2012+
+    Example usage: .\Get-WBReport.ps1
+#>
+
+# Public Varibles
 $MailMessageTo = "test@example.com" # List of users to email your report to (separate by comma)
 $MailMessageFrom = "test@example.com" # Enter the email you would like the report sent from
 $MailMessageSMTPServer = "mail.example.com" # Enter your own SMTP server DNS name / IP address here
 $MailMessagePriority = "Normal" # Low/Normal/High
 $HTMLMessageSubject = $env:computername+": Backup Report - "+(Get-Date) # Email Subject
-#endregion
 
 # DO NOT CHANGE ANYTHING PAST THIS LINE!
 
-# Variables
+# Private Variables
 $WBJob = Get-WBJob -Previous 1
 $WBSummary = Get-WBSummary
 $WBJobStartTime = $WBJob.StartTime
@@ -25,29 +28,12 @@ $WBJobSuccessLog = Get-Content -Path $WBJob.SuccessLogPath
 $WBJobFailureLog = Get-Content -Path $WBJob.FailureLogPath
 
 # Change Result of 0 to Success in green text and any other result as Failure in red text
-If ($WBSummary.LastBackupResultHR -eq 0) 
-{
-$HTMLMessageBody = @"
-<h1><font color=green>Backup Success</font></h1><br />
-Start Time: $WBJobStartTime<br />
-Start Time: $WBJobEndTime<br />
-<br />
-<b>Log:</b><br />
-<br />
-$WBJobSuccessLog
-"@
-}
-Else
-{
-$HTMLMessageBody = @"
-<h1><font color=red>Backup Failure</font></h1><br />
-Start Time: $WBJobStartTime<br />
-Start Time: $WBJobEndTime<br />
-<br />
-<b>Log:</b><br />
-<br />
-$WBJobFailureLog
-"@
+If ($WBSummary.LastBackupResultHR -eq 0) {
+    $WBJobResult = "successful"
+    $WBJobLog = $WBJobSuccessLog
+} Else {
+    $WBJobResult = "failed"
+    $WBJobFailureLog
 }
 
 # Assemble the HTML Report
@@ -56,9 +42,19 @@ $HTMLMessage = @"
 <html>
 <head>
 <title>$HTMLMessageSubject</title>
+<style>
+h1.successful {color:green;}
+h1.failed {color:red;}
+</style>
 </head>
 <body>
-$HTMLMessageBody
+<h1 class="$WBJobResult">Backup $WBJobResult</h1>
+Start Time: $WBJobStartTime<br>
+Start Time: $WBJobEndTime<br>
+<br>
+<p>Log:</p>
+<br>
+$WBJobLog
 </body>
 </html>
 "@
